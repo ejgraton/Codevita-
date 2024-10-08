4286427dece043e8d8d8731673d9d3a1395f676f
Evandro José Graton <ejgraton@gmail.com>
2024-10-08 17:31:00 -0300

    Create WaterCistern.java
    Adicionando fonte java

---
+++ 202311_Codevita/04_WaterCistern/WaterCistern.java
@@ -0,0 +1,38 @@
+//Codevita 2024
+//Cisterna d'água
+//Evandro José Graton
+
+import java.util.Scanner;
+import java.time.*;
+import java.time.format.*;
+
+public class WaterCistern {
+
+  private int r = 0, h = 0, s = 0, d = 0, g = 0;
+  private Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    new WaterCistern().execute();
+  }
+
+  public void execute() {
+
+    //Realiza a obtenção e validação das variáveis
+    entrada_rhs();
+    entrada_dg();
+
+    //Registra o inicio do calculo
+    ZonedDateTime t0 = ZonedDateTime.now();
+    
+  
+  }
+
+  private void entrda_rhs() {
+    
+  }
+
+  private void entrada_dg() {
+    
+  }
+    
+}
